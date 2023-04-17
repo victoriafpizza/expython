c2e5f02c08c8c682eced1806fadf372413031ab0
victoriafpizza <rm550609@fiap.com.br>
2023-04-17 10:45:08 -0300

    salvando exercicios

---
+++ jokenpo2.py
@@ -0,0 +1,12 @@
+import random 
+
+while escolha != 0 
+    print ("escolha: pedra, papel, ou tesoura")
+    jogador = input()
+    sorteio = random.randint(1,3)
+    if sorteio == 1
+        pc = "pedra"
+    else :
+        if sorteio == 2:
+            pc = "papel"
+        else
