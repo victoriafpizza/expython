1d0e6592dc8b61f0cc072542b676dabb9b83c452
victoriafpizza <rm550609@fiap.com.br>
2023-05-22 14:58:34 -0300

    salva

---
+++ 1sp/ex1.py
@@ -0,0 +1,5 @@
+print("hello word")
+
+print("Victoria Franceschini Pizza, 19 anos")
+
+print("Victoria Franceschini Pizza, " 2023-2004 " anos")
---
+++ 1sp/revisao.py
@@ -0,0 +1,14 @@
+# comentarios em python são feitos com #
+
+""" oioi
+tudo bem 
+com 
+voce?"""
+
+print("lero \n lero", 5)
+
+""" 
++ adição
+- subtração
+* multiplicação
+/ divisão """
